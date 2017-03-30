0d54622b271dfc32b47793b8091b0d5430435dfe
asiles <asilesp24@gmail.com>
2017-03-30 18:44:25 +0200

    script cambiado

--- 00-centosbasica/installepel.sh
+++ 00-centosbasica/installepel.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-yum update -y
-yum install -y epel-release
+#yum update -y
+#yum install -y epel-release
+
+touch ~/tmp
+echo "EPel Instalado! (en realidad no, pero tarda mil años)" >> ~/tmp
 
 #he arreglado
